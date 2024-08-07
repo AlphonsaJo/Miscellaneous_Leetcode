a4b24802181a9434d1c77f721cdcc27268cfc1ce
AlphonsaJo <91889313+AlphonsaJo@users.noreply.github.com>
2024-08-07 23:10:04 +0530

    Update Lambda_functions.py

--- Lambda_functions.py
+++ Lambda_functions.py
@@ -3,3 +3,7 @@ str = "test"
 upper = lambda string : string.upper()
 print(upper(str))
 
+#List Comprehension
+is_even_list = [lambda arg=x: arg * 10 for x in range(1, 5)]
+for item in is_even_list:
+    print(item())
