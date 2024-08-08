743774517e66211d8cea3637bf7a2a5557c3a649
AlphonsaJo <91889313+AlphonsaJo@users.noreply.github.com>
2024-08-09 00:45:03 +0530

    Update Lambda_functions.py

--- Lambda_functions.py
+++ Lambda_functions.py
@@ -7,3 +7,9 @@ print(upper(str))
 is_even_list = [lambda arg=x: arg * 10 for x in range(1, 5)]
 for item in is_even_list:
     print(item())
+    
+# if-else loop
+max = lambda a, b : a if (a > b) else b
+print(max(1,5))
+
+
