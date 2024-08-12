9a32e10b8c32c815249ed54cb8e3d142681e80e4
AlphonsaJo <91889313+AlphonsaJo@users.noreply.github.com>
2024-08-12 23:09:31 +0530

    Update Lambda_functions.py

--- Lambda_functions.py
+++ Lambda_functions.py
@@ -17,3 +17,8 @@ print(max(1,5))
 sortList = lambda x: (sorted(i) for i in x)
 secondLargest = lambda x, f : [y[len(y)-2] for y in f(x)]
 res = secondLargest(List, sortList)
+
+#Filter
+li = [5, 7, 22, 97, 54, 62, 77, 23, 73, 61]
+final_list = list(filter(lambda x: (x % 2 != 0), li))
+print(final_list)
