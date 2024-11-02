95c2233e79bb5154ce25c61b7bf4dd4ee7038e93
AlphonsaJo <91889313+AlphonsaJo@users.noreply.github.com>
2024-11-03 01:08:30 +0530

    Time: 386 ms (78.87%), Space: 70.8 MB (13.93%) - LeetHub

--- 3072-reshape-data-pivot/3072-reshape-data-pivot.py
+++ 3072-reshape-data-pivot/3072-reshape-data-pivot.py
@@ -1,10 +1,8 @@
 import pandas as pd
 
 def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
-    # Pivot the DataFrame
+
     pivoted_df = weather.pivot_table(index='month', columns='city', values='temperature', aggfunc = 'max')
-    
-    # Resetting the index to have 'month' as a column again
   
     
     return pivoted_df
