0174220fbc97dd191383d6d8b29cb8ee8a99bda8
aj24 <91889313+AlphonsaJo@users.noreply.github.com>
2025-07-25 10:40:26 +0530

    Time: 0 ms (100%), Space: 40.4 MB (65.4%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,25 +1,23 @@
-
 class Solution {
-
-
-    
     public int climbStairs(int n) {
-        
-        if (n <= 0) return 0; // Invalid/Edge Case
-        if (n == 1) return 1; // Base Case
-        if (n == 2) return 2; // Two ways: One 2-steps or 2 1-steps
+        //initialize size (n+1) to include dp[n]
+        int dp[] = new int[n+1];
 
-        int[] dp = new int[n+1];
-        dp[0] = 1; // one way to stay at ground level
+        //handle edge cases
+        if(n<=1)
+            return 1;
+
+        //base cases
+        dp[0] = 1;
         dp[1] = 1;
-        dp[2] = 2;
 
-        for (int i = 3; i<=n; i++){
+        // Bottom-up DP computation
+        for(int i=2; i<=n; i++){
             dp[i] = dp[i-1] + dp[i-2];
         }
 
-        return dp[n];
-
+        //store and return result
+        int result = dp[n];
+        return result;
     }
-    
 }
