7abf345b503b3e697903c919d68c0a8ab86d43a5
aj24 <91889313+AlphonsaJo@users.noreply.github.com>
2025-07-31 10:42:00 +0530

    Time: 0 ms (100%), Space: 40.6 MB (57.6%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.java
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.java
@@ -9,9 +9,9 @@ class Solution {
 
         // dp[i] will store the number of ways to tile a 2 x i board
         long[] dp = new long[n + 1];
-        dp[0] = 1;  // Empty board
-        dp[1] = 1;  // Only 1 vertical domino
-        dp[2] = 2;  // Two vertical or two horizontal dominoes
+        dp[0] = 1;  
+        dp[1] = 1; 
+        dp[2] = 2;  
 
         for (int i = 3; i <= n; i++) {
             dp[i] = (2 * dp[i - 1] + dp[i - 3]) % MOD;
